e4a8d6b83a6422b983641325f491cb9f0b332d84
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2015-05-22 13:56:05 -0400

    list of retailers on UI

--- app/assets/javascripts/Home/HomeController.js
+++ app/assets/javascripts/Home/HomeController.js
@@ -1,7 +1,25 @@
-app.controller("HomeController", ['$scope', function ($scope) {
+app.controller("HomeController", function ($scope, $http) {
     $scope.zipCode = '';
+    $scope.retailers = [];
 
     $scope.searchForZip = function () {
-        $location.search({ zipCode: $scope.zipCode });
     };
-}]);
+
+
+    $scope.geocodeResultPostalCode = function (data, status, headers, config) {
+        // If there are results, extract the latitude and longitude of the zip code's center
+        if (data.retailers.length > 0) {
+            $scope.retailers = data.retailers;
+            console.log($scope.retailers);
+        }
+        else {
+            alert('Could not locate zip code: ' + $scope.zip);
+        }
+    };
+
+    $http.get('http://localhost:3000/retailers.json', {
+        params: { components: 'postal_code:' + $scope.zipCode },
+    }).success($scope.geocodeResultPostalCode)
+      .error(function (data) { alert('doh'); });
+
+});
