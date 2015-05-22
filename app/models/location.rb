f92e4cf0b461457fa7e968e6d1cb783b10530967
Jim Gay <jim@saturnflyer.com>
2015-05-22 15:25:29 -0400

    use keywords for location initialization

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 require 'geokit/lat_lng'
 require 'delegate'
 class Location < DelegateClass(Geokit::LatLng)
-  def initialize(lat=38.8976757,lng=-77.036528)
+  def initialize(lat: 38.8976757,lng: -77.036528)
     super(Geokit::LatLng.new(lat, lng))
   end
 end
