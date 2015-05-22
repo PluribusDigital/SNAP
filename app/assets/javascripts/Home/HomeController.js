eb68a88231c2487073abc4220783a7cf54589e7a
Jeffrey M Farley <JeffreyMFarley@users.noreply.github.com>
2015-05-22 14:33:45 -0400

    Lat Long in the UI

--- app/assets/javascripts/Home/HomeController.js
+++ app/assets/javascripts/Home/HomeController.js
@@ -1,10 +1,25 @@
 app.controller("HomeController", function ($scope, $http) {
     $scope.zipCode = '';
     $scope.retailers = [];
+    $scope.enableLocation = ('geolocation' in navigator);
+    $scope.lat = 40.7637828;
+    $scope.lng = -73.9859116;
+
+    $scope.searchFromHere = function () {
+        navigator.geolocation.getCurrentPosition($scope.currentPosition);
+    }
+
+    $scope.currentPosition = function(position) {
+        $scope.lat = position.coords.latitude;
+        $scope.lng = position.coords.longitude;
+    }
 
     $scope.searchForZip = function () {
+        $http.get('http://localhost:3000/retailers.json', {
+            params: { components: 'postal_code:' + $scope.zipCode },
+        }).success($scope.geocodeResultPostalCode)
+          .error(function (data) { alert('doh'); });
     };
-
 
     $scope.geocodeResultPostalCode = function (data, status, headers, config) {
         // If there are results, extract the latitude and longitude of the zip code's center
@@ -16,10 +31,4 @@ app.controller("HomeController", function ($scope, $http) {
             alert('Could not locate zip code: ' + $scope.zip);
         }
     };
-
-    $http.get('http://localhost:3000/retailers.json', {
-        params: { components: 'postal_code:' + $scope.zipCode },
-    }).success($scope.geocodeResultPostalCode)
-      .error(function (data) { alert('doh'); });
-
 });
